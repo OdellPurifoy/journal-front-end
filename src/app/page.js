3864de3623d3bd85b3db212016d0965292e84ba8
Odell Purifoy <opurifoy@springhealth.com>
2023-12-10 17:46:04 -0500

    Successfully fetched and displayed journal data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,41 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedJournals, setLoadedJournals] = useState([]);
+  const journalData = loadedJournals?.journals?.data
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch("http://localhost:4000//api/v1/journals").then((response) => {
+      return response.json();
+    }).then((data) => {
+      setIsLoading(false);
+      setLoadedJournals(data)
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  
   return (
     <main>
-      <h1>Hello</h1>
+      <ul>
+        {journalData.map((journal) => (
+          <li key={journal.id}>
+            {journal.attributes.title}
+            {journal.attributes.kind}
+          </li>
+        ))}
+      </ul>   
     </main>
   )
 }
